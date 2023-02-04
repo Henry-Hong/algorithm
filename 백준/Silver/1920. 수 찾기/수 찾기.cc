76dd29779a9704aceee316d98bb54e2fb63062e8
heerim <hhr8540@naver.com>
2023-02-04 20:24:51 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,51 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
+int N, M, x;
 int arr[100001];
 
-int biSearch(int s, int e, int cmp) {
-	
-	if (s > e) return 0;
+int binarySearch(int x) {
 
-	int result = 0;
-	int mid = (s + e) / 2;
+  int left = 0, right = N - 1;
 
-	if (arr[mid] > cmp) {
-		result = biSearch(s, mid - 1, cmp);
-	}
-	else if (arr[mid] < cmp) {
-		result = biSearch(mid + 1, e, cmp);
-	}
-	else { // 같음
-		result = 1;
-	}
-
-	return result;
-}
-
-int compare(const void* a, const void* b) {
-	return *(int*)a > *(int*)b;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (x == arr[mid])
+      return 1;
+    else if (x < arr[mid])
+      right = mid - 1;
+    else if (x > arr[mid])
+      left = mid + 1;
+    else
+      cout << "not reachable";
+  }
+  return 0;
 }
 
 int main() {
 
-	int n, m, cmp;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
+  cin.tie(NULL);
+  ios::sync_with_stdio(false);
 
-	//정렬
-	qsort(arr, n, sizeof(int), compare);
-	   	
-	scanf("%d", &m);
-	for (int i = 0; i < m; i++) {
+  cin >> N;
+  for (int i = 0; i < N; i++)
+    cin >> arr[i];
 
-		scanf("%d", &cmp);
+  sort(arr, arr + N);
 
-		if (biSearch(0,n-1,cmp)) printf("1\n");
-		else printf("0\n");
-	}
-	
-	return 0;
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    cin >> x;
+    cout << binarySearch(x) << "\n";
+  }
+
+  return 0;
 }
